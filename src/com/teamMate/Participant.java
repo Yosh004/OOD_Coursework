5950a6205b62165b75ccd01359f94e685a5556e7
Yosh004 <yoshithavithanage@gmail.com>
2025-11-09 14:01:46 +0530

    Initial commit

--- src/com/teamMate/Participant.java
+++ src/com/teamMate/Participant.java
@@ -1,6 +1,22 @@
 package com.teamMate;
 
 public class Participant {
+    private String studentID;
+    private String name;
+    private int personalityScore;
+    private String personalityType; //leader etc.
+    private String preferredGame;
+    private String preferredRole; //defender etc.
 
+    //create new participant constructor
+    public Participant(String studentID,String name,int personalityScore,String personalityType,String preferredGame,String preferredRole) {
+        this.studentID=studentID;
+        this.name=name;
+        this.personalityScore=personalityScore;
+        this.personalityType=personalityType;
+        this.preferredGame=preferredGame;
+        this.preferredRole=preferredRole;
+
+    }
 
 }

43b9fe528d1604d89bb2e6e8406b579a514e7939
Yosh004 <yoshithavithanage@gmail.com>
2025-11-09 17:16:49 +0530

    Initial commit

--- src/com/teamMate/Team.java
+++ src/com/teamMate/Team.java
@@ -1,4 +1,50 @@
 package com.teamMate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Team {
+    private List<Participant> members;
+    private int teamID;
+
+    private static int nextID = 1; //Static counter for unique team ids
+
+    public Team(){
+        this.members = new ArrayList<Participant>();
+        this.teamID = nextID++;
+    }
+    public void addMember(Participant p){
+        this.members.add(p);
+    }
+    public List<Participant> getMembers(){
+        return this.members;
+    }
+    public int getTeamID(){
+        return this.teamID;
+    }
+    public int getSize(){
+        return members.size();
+    }
+
+
+    public boolean hasRole(String role) {
+        for (Participant member : members) {
+            if (member.getPreferredRole().equalsIgnoreCase(role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        String memberNames = members.stream()
+                .map(Participant::getName)
+                .collect(Collectors.joining(", "));
+        return "Team #" + teamID + ": [" + memberNames + "]";
+
+    }
+
+
 }
